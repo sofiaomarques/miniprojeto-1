64df1929637ec3b59aa86c48ef8188a3e00f12dd
Sofia de Oliveira Marques <sofiaomarques26@gmail.com>
2026-08-06 14:52:44 -0300

    Avancos

--- catalogo.py
+++ catalogo.py
@@ -4,7 +4,11 @@ class Catalogo:
     def __init__(self, caminho_json: str):
         with open(caminho_json, encoding="utf-8") as arquivo:
             dados = json.load(arquivo)
-        self.conteudos = dados["conteudos"]
-        self.usuarios = dados["usuarios"]
-        self.conteudos_por_id = {c["id"]: c for c in self.conteudos}
-        self.usuarios_por_id = {u["id"]: u for u in self.usuarios}
+        self.plataforma=dados["plataforma"]
+        self.meta=dados["meta"]
+        self.conteudos=dados["conteudos"]
+        self.usuarios=dados["usuarios"]
+    def mostrar (self):
+        print(self.dados)
+cat1= Catalogo("/catalogo_dev.json")
+cat1.mostrar()
